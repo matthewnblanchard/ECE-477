4db9edaee0f6ccbefd23c0827e7c9ae5c8cfa926
christianscott0482 <christianscott0482@yahoo.com>
2017-02-09 16:39:56 -0500

    First commit of trinary conversion function.

--- vtic.c
+++ vtic.c
@@ -1,10 +1,15 @@
 
 #include <stdio.h>
+#include <math.h>
 
 #define ERR_ARGC 1 << 0
 
-int main(int argc, char *argv[]) {
+int convert(int game, char *gamelist);
 
+int main(int argc, char *argv[]) {
+	
+	unsigned int tri = 0;
+	char trilist[9];
         // Argument checking
         if (argc > 2) {
                 printf("Error: too many arguments");
@@ -13,3 +18,19 @@ int main(int argc, char *argv[]) {
 
         return 0;
 }
+
+int convert(int game, char *gamelist){
+
+	if( game > 19683)		//tests if game number is within the maximum (3^9)
+		return 1;		//returns 1 if game number is too large
+
+	char i;				//counter
+	char digit;			//temporary value that has the current digit
+	for( i = 0; i < 9; i++){	//calculates the value of each digit
+		digit - 0;
+		digit = game / (pow(3,(i + 1));//eliminate values below specified digit
+		digit = digit % 3;		//eliminate values above specified digit
+		gamelist[i] = digit;	//store digit
+	}
+	return 0;	
+}			
