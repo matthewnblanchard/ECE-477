baf5cc0ce5c15293f239638854934e978fbab74f
christianscott0482 <christianscott0482@yahoo.com>
2017-02-24 04:29:52 -0500

    Commit, it's late

--- Lab_3/timer.c
+++ Lab_3/timer.c
@@ -2,13 +2,17 @@
 #include <unistd.h>
 #include <sys/io.h>
 
-int init(void);
+#define CLOCK_FREQ 1192500.0
 
 int main(int argc, char *argv[]){
 
 	int final_t = 0;
 	int initial_t = 0;
 	int port_val = 0;
+	double diff = 0;
+	FILE *fp;
+	int i = 0;
+	int j = 0;	
 
 	// Intialization
 	// Set port permissions
@@ -24,21 +28,37 @@ int main(int argc, char *argv[]){
 	// Configure timer channel 2 to output in binary, outputing
 	// lower bits then upper bits	
 	outb(0xB4, 0x43);
-	printf("0x43 reads: %x\n", inb(0x43));
 	
-	// Read initial time value
-	initial_t = inb(0x42);
-	initial_t |= (inb(0x42) << 4);
+	// Set reload value
+	outb(0xFF, 0x42);
+	outb(0xFF, 0x42);	
 
-	// Sleep for a bit
-	usleep(100000);
+	fp = fopen("out.txt", "w");
+	for (i = 1; i <= 2000; i++) {
+		for (j = 0; j < 10; j++) {	
+			// Read initial time value
+			outb(0x08, 0x43);        // Send latch command
+			initial_t = inb(0x42);
+			initial_t |= (inb(0x42) << 8);
 
-	// Read final time value
-	final_t = inb(0x42);
-	final_t |= (inb(0x42) << 4);
+			// Sleep for a bit
+			usleep(i);
 
-	// Print results
-	printf("initial: %d\nfinal: %d\n", initial_t, final_t);
+			// Read final time value
+			outb(0x08, 0x43);	// Send latch command
+			final_t = inb(0x42);
+			final_t |= (inb(0x42) << 8);
+
+			// Calculate time
+			diff = initial_t - final_t;
+			if (diff <= 0) { 	// A rollover must have occured
+				diff = initial_t + (0xFFFF - final_t);
+			}
+			diff /= CLOCK_FREQ;
+			diff *= 1000000;
+			fprintf(fp, "%d,%f\n", i, diff);
+		}
+	}
 	return 0;
 }
 
